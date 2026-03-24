da26a8905c64646df95e5d63c51cceec194d7042
jelledijkema <jelle@dijkema-analytics.nl>
2026-03-24 13:21:04 +0100

    redundant test

--- tests/test_fme_import.py
+++
@@ -1,6 +0,0 @@
-from profielentoolbox.hello_world import hello_world
-
-def test_import():
-    """Test de import van de hello world functie."""
-    result = hello_world()
-    assert result == "Hello world again!"
